b05b44f642cd50925aa62cc2838df1865c7e8494
Torben Zillerr <torbenziller@web.de>
2025-03-14 21:15:37 +0100

    Brutto Netto Rechner

---
+++ Controller/CalcTax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GymClock.Controller
+{
+    public class CalcTax
+    {
+        public static double BruttoToNetto(string land, double bamount, bool spezial)
+        {
+            double tax = 0.0;
+            switch (land)
+            {
+                case "DE":
+                    tax = spezial ? 0.07 : 0.19;
+                    break;
+                case "AT":
+                    tax = spezial ? 0.07 : 0.20;
+                    break;
+            }
+
+            double namount = bamount * (1 + tax);
+            return namount;
+        }
+    }
+}
